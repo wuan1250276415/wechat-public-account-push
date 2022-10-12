cc79b24b7d23c4b73224dd2252dc6f8ed1f74b5f
wuan1250276415 <33180185+wuan1250276415@users.noreply.github.com>
2022-10-12 14:26:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小熊宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oYm4v5vti8CNBNSgoeLrXTmhG30A',
+      id: 'oYm4v5ogecXf_LhJlgCPAZ5jFcFs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Eqdrg3NYk2UcSoJ5r1r03Jl6_dPKKmsdity1dogQGCg',
+      useTemplateId: '61qaugS5qT0Boo-yMt1IZ4rYVpOzPq2_pr-QYODsBeM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-25',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '61qaugS5qT0Boo-yMt1IZ4rYVpOzPq2_pr-QYODsBeM',
 
   CALLBACK_USERS: [
     {
